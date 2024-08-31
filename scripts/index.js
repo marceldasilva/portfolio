d22e9092d7871a091d371f95bc3886cb6492f421
Marcel <marcelalvessc@gmail.com>
2024-08-31 18:39:21 -0300

    organizando o menu

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+const menu = document.querySelector(".menu");
+const itens = document.querySelector(".itens");
+
+body.addEventListener('onresize', () => mudarTamanho());
+menu.addEventListener('click',() => clickMenu());
+
+function clickMenu() {
+    if(itens.style.display === 'none') {
+        itens.style.display = 'block';
+    } else {
+        itens.style.display = 'none';
+    }
+}
+
+function mudarTamanho() {
+    if(window.innerWidth >= 768) {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
