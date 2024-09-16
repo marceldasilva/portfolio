1e232925e74b288ed551c502df8a15222f016b8e
Marcel <marcelalvessc@gmail.com>
2024-09-16 00:22:16 -0300

    fazendo a media query

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
 const menu = document.getElementById("burger");
 const itens = document.querySelector(".itens");
+const body = document.querySelector("body");
 
 menu.addEventListener('click', () => itensOn());
+body.addEventListener('resize', () => mudouTamanho());
 
 
 function itensOn() {
@@ -9,5 +11,13 @@ function itensOn() {
         itens.style.display = 'none';
     } else {
         itens.style.display = 'block';
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block';
+    } else {
+        itens.style.display = 'none';
     }
 }
